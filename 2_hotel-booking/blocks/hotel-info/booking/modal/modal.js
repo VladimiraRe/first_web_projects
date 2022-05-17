a517e45cf6896c7c2fd740c1a39c6bba0904bc97
VladimiraRe <vladimira_ra@mail.ru>
2022-05-17 19:21:33 +0300

    Created modal section and added styles for it

---
+++ 2_hotel-booking/blocks/hotel-info/booking/modal/modal.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+
+    const modal = document.querySelector('.modal')
+    const modal__close = document.querySelector('.modal__close')
+
+    modal__close.addEventListener('click', function() {
+        console.log('Click')
+    })
+
+
+
+
+
+})
